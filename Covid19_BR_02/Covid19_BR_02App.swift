e8bb6acdbe9f28c7e2b84d86f9a022ba7e240c0b
Nazildo Souza <nsouza10@outlook.com>
2020-07-24 15:42:42 -0300

    Initial Commit

---
+++ Covid19_BR_02/Covid19_BR_02App.swift
@@ -0,0 +1,17 @@
+//
+//  Covid19_BR_02App.swift
+//  Covid19_BR_02
+//
+//  Created by Nazildo Souza on 24/07/20.
+//
+
+import SwiftUI
+
+@main
+struct Covid19_BR_02App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
